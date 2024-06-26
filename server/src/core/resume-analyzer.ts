f2ae67fbb81ed398b2af621ad020fcb486bf4cc3
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-26 15:15:26 +0530

    server/core: add stubs for resume-analyzer

--- server/src/core/resume-analyzer.ts
+++ server/src/core/resume-analyzer.ts
@@ -1,5 +1,67 @@
 class ResumeAnalyzer {
-  constructor() {}
+  private fileKey: string;
+  private resumeText: string;
+
+  constructor(fileKey: string) {
+    this.fileKey = fileKey;
+    this.resumeText = "";
+  }
+
+  async fetchResume() {
+    console.log(this.resumeText);
+    throw new Error("Method not implemented.");
+  }
+
+  async parseResume() {
+    this.resumeText = "This is a sample resume text.";
+    throw new Error("Method not implemented.");
+  }
+
+  async getPresentationScore() {
+    return {
+      presentation: {
+        score: 0.8,
+        message: "Your resume is well presented.",
+      },
+    };
+  }
+
+  async getConcisenessScore() {
+    return {
+      conciseness: {
+        score: 0.6,
+        message: "Your resume is concise.",
+      },
+    };
+  }
+
+  async getRelevanceScore() {
+    return {
+      relevance: {
+        score: 0.9,
+        message: "Your resume is relevant.",
+      },
+    };
+  }
+
+  async getKeywordsAnalysis() {
+    return {
+      keywords: {
+        score: 0.7,
+        message: "Your resume contains relevant keywords.",
+        keywords_detected: ["Java", "Python", "JavaScript"],
+      },
+    };
+  }
+
+  getImpactScore() {
+    return {
+      impact: {
+        score: 0.8,
+        message: "Your resume is impactful.",
+      },
+    };
+  }
 }
 
 export { ResumeAnalyzer };

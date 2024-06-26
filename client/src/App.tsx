9ec61391a60efa208720cc5b953c2ed4767f9b53
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-27 01:35:11 +0530

    client: add file upload button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Button } from "@nextui-org/button";
 
 function App() {
+  const inputFile = useRef(null);
+
   useEffect(() => {
     document.title = "Resume Analyzer";
   }, []);
+
+  const handleUploadClick = (): void => {
+    if (inputFile.current) {
+      const input = inputFile.current as HTMLInputElement;
+      input.click();
+    }
+  };
+
+  const handleFileChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const file = event.target.files?.[0];
+    if (file) {
+      console.log(file);
+    }
+  };
 
   return (
     <div
@@ -25,8 +43,15 @@ function App() {
         Crack your dream job by optimizing your resume with Resume Analyzer. ✅
       </p>
       <div className="flex gap-4 mt-4 justify-center">
-        <Button color="primary" size="lg">
+        <Button color="primary" size="lg" onClick={handleUploadClick}>
           Upload Resume
+          <input
+            onChange={handleFileChange}
+            className="hidden"
+            type="file"
+            id="file"
+            ref={inputFile}
+          />
         </Button>
         <Button color="primary" size="lg">
           View Analyzed Resumes

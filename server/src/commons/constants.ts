3a8b6b60efd776c881246e8d382f7c7635de5359
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-27 02:04:37 +0530

    server: set APP_PORT to 8000

--- server/src/commons/constants.ts
+++ server/src/commons/constants.ts
@@ -1,5 +1,5 @@
 /** Note: Please change this according to the requirements of your project */
 const APP_NAME = "Resume Analyzer";
-const APP_PORT = 3000;
+const APP_PORT = 8000;
 
 export { APP_NAME, APP_PORT };

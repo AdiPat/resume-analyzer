2402ac9934ade2511a26c121a8a58ab998d7b13c
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-27 06:45:07 +0530

    server/core: implement fetch, parse and getResumeText

--- server/src/core/resume-analyzer.ts
+++ server/src/core/resume-analyzer.ts
@@ -1,15 +1,63 @@
+import { s3 } from "../commons/config";
+import pdf from "pdf-parse";
+
+
 class ResumeAnalyzer {
   private fileKey: string;
   private resumeText: string;
+  private dataBuffer: Buffer | null;
 
   constructor(fileKey: string) {
     this.fileKey = fileKey;
     this.resumeText = "";
+    this.dataBuffer = null;
+  }
   }
 
-  async fetchResume() {
+  async fetchResume(): Promise<void> {
+    if (this.dataBuffer) {
+      return;
+    }
+
+    this.dataBuffer = await s3
+      .getObject({
+        Bucket: process.env.AWS_BUCKET_NAME ?? "",
+        Key: this.fileKey,
+      })
+      .promise()
+      .then((data) => data.Body as Buffer)
+      .catch((err) => {
+        console.error("failed to fetch resume from s3");
+        return null;
+      });
+  }
+
+  async parseResume(): Promise<string> {
+    if (!this.dataBuffer) {
+      await this.fetchResume();
+    }
+
+    if (!this.dataBuffer) {
+      throw new Error("Failed to fetch resume data.");
+    }
+
+    const data = await pdf(this.dataBuffer).catch((err) => {
+      console.error("failed to parse resume", err);
+      return null;
+    });
+
+    if (!data) {
+      throw new Error("Failed to parse resume data.");
+    }
+
+    this.resumeText = data.text;
+
+    return this.resumeText;
+  }
+
+  async getResumeText(): Promise<string> {
     console.log(this.resumeText);
-    throw new Error("Method not implemented.");
+    return this.resumeText;
   }
 
   async parseResume() {

0f62c7bc11540f4bc601993d56e12665f59378f0
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-27 06:45:46 +0530

    server/tests: add resume-analyzer tests

---
+++ server/src/core/resume-analyzer.test.ts
@@ -0,0 +1,25 @@
+import { ResumeAnalyzer } from "./resume-analyzer";
+
+const SAMPLE_RESUME_KEY = "resumes/4Jfd7Jnb98GJtCiW4Hzoo_ADITYA_RESUME.pdf";
+
+jest.setTimeout(30000);
+
+describe("resume-analyzer", () => {
+  it("should parse a resume correctly", async () => {
+    // s3://resume-analyzer-0001/resumes/4Jfd7Jnb98GJtCiW4Hzoo_ADITYA_RESUME.pdf
+    const resumeAnalyzer = new ResumeAnalyzer(SAMPLE_RESUME_KEY);
+    await resumeAnalyzer.parseResume();
+    const resumeText = await resumeAnalyzer.getResumeText();
+    expect(resumeText.includes("Tech Lead")).toBeTruthy();
+  });
+
+  it("should have a presentation score", async () => {
+    const resumeAnalyzer = new ResumeAnalyzer(SAMPLE_RESUME_KEY);
+    await resumeAnalyzer.parseResume();
+    const presentationScore = await resumeAnalyzer.getPresentationScore();
+    console.log({ presentationScore });
+    expect(presentationScore).toHaveProperty("presentation");
+    expect(presentationScore.presentation).toHaveProperty("score");
+    expect(presentationScore.presentation).toHaveProperty("description");
+  });
+});
